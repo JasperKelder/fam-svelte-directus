d847ee621899daf3314cd0a0978374c41ec3445a
jasper.kelder <jasperkelder@gmail.com>
2022-07-17 20:04:55 +0200

    Update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,19 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').Load} */
+	export const load = async ({ url: { pathname } }) => ({
+		props: { pathname }
+	});
+</script>
+
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
 	import '../main.css';
+	import PageTransition from '$lib/components/PageTransition.svelte';
+	export let pathname;
 </script>
 
 <Header />
-<slot />
+<PageTransition {pathname}>
+	<slot />
+</PageTransition>
